51e5f54a5bd75e74c12e71fc00b1cf35c8790a89
rasakmarsawa <rasakmarsawa@gmail.com>
2025-09-30 21:33:33 +0700

    make new route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Making 2nd route with view
+// Route::get('/second', function () {
+//     return view('second');
+// });
+
+//Making 3rd route but no view
+Route::get('/third', function () {
+    return view('third');
+});
+
+//Simpler code for routing without logic usage
+Route::view('/second', 'second');
+
+
