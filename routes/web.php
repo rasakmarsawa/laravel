7af1b435d81ff7d8672966dbcae0eb1250d6de81
rasakmarsawa <rasakmarsawa@gmail.com>
2025-10-01 07:30:47 +0700

    make new page and successfully load tailwind using vite

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-//Making 2nd route with view
-// Route::get('/second', function () {
-//     return view('second');
-// });
-
 //Making 3rd route but no view
 Route::get('/third', function () {
     return view('third');
@@ -18,5 +9,7 @@ Route::get('/third', function () {
 
 //Simpler code for routing without logic usage
 Route::view('/second', 'second');
+
+Route::view('/', 'home');
 
 
