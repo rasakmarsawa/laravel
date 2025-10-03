4255e7e1d23e7c587511fbc34f9bb5ab28af7ee8
rasakmarsawa <rasakmarsawa@gmail.com>
2025-10-03 19:47:08 +0700

    eloquent is crazy

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 // use Illuminate\Support\Facades\DB;
 use App\Models\Category;
+use App\Models\Post;
 
 class HomeController extends Controller
 {
@@ -13,6 +14,21 @@ class HomeController extends Controller
     {
         // $categories = DB::table('categories')->get();
         $categories = Category::all();
-        return view('home', ['categories' => $categories]);
+        // $posts = Post::orderBy('id','desc')->get();
+
+        // $posts = Post::latest()->get();
+
+        // $posts = Post::where('category_id', request('category_id'))
+        // ->latest()
+        // ->get();
+
+        $posts = Post::when(request('category_id'), function($query){
+            $query->where('category_id', request('category_id'))
+            ->latest()
+            ->get();
+        })->latest()->get();
+
+        // return view('home', ['categories' => $categories, 'posts' => $posts]);
+        return view('home', compact('categories','posts'));
     }
 }
