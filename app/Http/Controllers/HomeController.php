9d26bf07e0a1496d177e7286486102343b116a6c
rasakmarsawa <rasakmarsawa@gmail.com>
2025-10-03 21:27:05 +0700

    cleaning code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-// use Illuminate\Support\Facades\DB;
 use App\Models\Category;
 use App\Models\Post;
 
@@ -12,15 +11,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        // $categories = DB::table('categories')->get();
         $categories = Category::all();
-        // $posts = Post::orderBy('id','desc')->get();
-
-        // $posts = Post::latest()->get();
-
-        // $posts = Post::where('category_id', request('category_id'))
-        // ->latest()
-        // ->get();
 
         $posts = Post::when(request('category_id'), function($query){
             $query->where('category_id', request('category_id'))
@@ -28,7 +19,6 @@ class HomeController extends Controller
             ->get();
         })->latest()->get();
 
-        // return view('home', ['categories' => $categories, 'posts' => $posts]);
         return view('home', compact('categories','posts'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,26 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 
-//Making 3rd route but no view
 Route::get('/third', function () {
     return view('third');
 });
 
-//Simpler code for routing without logic usage
 Route::view('/second', 'second');
 
-// Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
 Route::view('/article', 'article')->name('article');
-
-// get data and passing it in route logic, no controller
-// Route::get('/', function () {
-//     $categories = DB::table('categories')->get();
-//     return view('home', ['categories' => $categories]);
-// })->name('home');
-
-// Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
